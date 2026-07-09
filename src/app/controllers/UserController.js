966572c8e161b07bfdffb23c6fbd696ea117e8b9
Victor-Avilla <victorfonsecaavila@gmail.com>
2026-07-09 11:21:41 -0300

    feat: validação de email unico + feedback para usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,6 +13,12 @@ class UserController {
   async store(req, res) {
     const { name, email, password_hash, admin } = req.body;
 
+    const existingUser = await User.findOne({where:{email}})
+
+    if(existingUser){
+      return res.status(400).json({message:"Este email ja esta cadastrado, tente outro."})
+    }
+
     const user = await User.create({
       id: v4(),
       name,
