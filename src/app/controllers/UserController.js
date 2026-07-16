51b754d41e4acfa7216784fb319313f48f2a24d0
Victor-Avilla <victorfonsecaavila@gmail.com>
2026-07-16 12:05:05 -0300

    feat: validação de dados com Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,15 +8,30 @@
 
 import { v4 } from 'uuid';
 import User from '../models/User.js';
-
+import * as Yup from 'yup';
 class UserController {
   async store(req, res) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().min(6).required(),
+      admin: Yup.boolean().required(),
+    });
+    try{
+      schema.validateSync(req.body, { abortEarly: false, strict: true });}catch(err){
+        console.log(err);
+        return res.status(400).json({ message: err.errors });
+      }
+    
+
     const { name, email, password_hash, admin } = req.body;
 
-    const existingUser = await User.findOne({where:{email}})
+    const existingUser = await User.findOne({ where: { email } });
 
-    if(existingUser){
-      return res.status(400).json({message:"Este email ja esta cadastrado, tente outro."})
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: 'Este email ja esta cadastrado, tente outro.' });
     }
 
     const user = await User.create({
